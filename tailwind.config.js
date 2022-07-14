2b86949fba0f9073e3248b557619eff79577d8b2
emmens4u <emmanuel.mensah@amalitech.org>
2022-07-14 14:06:53 +0800

    Contact Form

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: {
     content: ['./public/**/*.html'],
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors:{
+      amber: colors.amber
+    },
+    extend: {
+      
+    },
   },
   variants: {
     extend: {},
